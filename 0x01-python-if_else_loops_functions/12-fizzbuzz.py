5d4ea075f4e861c7b29c3e9c343116777f27a36a
iVGeek <48223278+iVGeek@users.noreply.github.com>
2022-11-30 02:16:39 +0300

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-FIZZ = "Fizz"
-BUZZ = "Buzz"
+
+mot = ""
 
 
 def fizzbuzz():
-    for number in range(1, 101):
-        if (number % 3 and number % 5):
-            print("%s%s" % (FIZZ, BUZZ), end=' ')
-        elif (number % 3):
-            print("%s" % (FIZZ), end=' ')
-        elif (number % 5):
-            print("%s" % (BUZZ), end=' ')
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            mot = "FizzBuzz"
+        elif num % 3 == 0:
+            mot = "Fizz"
+        elif num % 5 == 0:
+            mot = "Buzz"
         else:
-            print("%d" % (number), end=' ')
+            mot = str(num)
+        print(mot, end=" ")
