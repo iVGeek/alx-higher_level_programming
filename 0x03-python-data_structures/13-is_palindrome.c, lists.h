b63a5107337a46b8dbcc3dadb65df9387cd74eb4
iVGeek <48223278+iVGeek@users.noreply.github.com>
2022-12-04 14:08:53 +0300

    13-is_palindrome.c, lists.h
    a function in C that checks if a singly linked list is a palindrome.

---
+++ 0x03-python-data_structures/13-is_palindrome.c, lists.h
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - a function
+ * @head: the list
+ *
+ * Return: 0 or 1
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *s = NULL, *e = NULL, *tmp = NULL;
+
+	s = *head;
+	while (s != tmp && s != e)
+	{
+		tmp = e;
+		e = s;
+		while (e->next != tmp)
+			e = e->next;
+
+		if (s->n != e->n)
+			return (0);
+		s = s->next;
+	}
+	return (1);
+}
