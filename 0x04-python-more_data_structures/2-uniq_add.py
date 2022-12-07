2383b9959aa024181da3def4111fdc02714e765c
iVGeek <48223278+iVGeek@users.noreply.github.com>
2022-12-07 21:54:23 +0300

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-
 def uniq_add(my_list=[]):
-    if my_list:
-        black_list = []
-        som = 0
-        for n in my_list:
-            if n not in black_list:
-                som += n
-                black_list.append(n)
-        return(som)
+    new_list = set(my_list)
+    sum = 0
+    for i in new_list:
+        sum += i
+    return
