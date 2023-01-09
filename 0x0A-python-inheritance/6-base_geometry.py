ad027c43260dcbaac3f400b3e215e68d1f235ea4
iVGeek <48223278+iVGeek@users.noreply.github.com>
2023-01-09 20:59:37 +0300

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Contains the class BaseGemetry
+"""
+
+
+class BaseGeometry:
+    """ The class empty"""
+
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
