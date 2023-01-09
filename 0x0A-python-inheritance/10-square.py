61384cc1814cf5231b8a52c96cf9a8db13712176
iVGeek <48223278+iVGeek@users.noreply.github.com>
2023-01-09 20:09:14 +0300

    10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Contains the class BaseGemetry
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ The class empty"""
+
+    def __init__(self, size):
+        """A representation of a square"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        return "[Rectangle] {:d}/{:d}".format(self.__size, self.__size)
+
+    def area(self):
+        return (self.__size ** 2)
