8b1a3428139c8c71b6a7048722a37c4ae47f23b2
iVGeek <48223278+iVGeek@users.noreply.github.com>
2023-01-10 20:49:03 +0300

    2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+append file function
+"""
+
+
+def append_write(filename="", text=""):
+    """The append file function"""
+
+    with open(filename, 'a', encoding="utf-8") as f:
+        f.write(text)
+    return (len(text))
