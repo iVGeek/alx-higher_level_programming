16f6bc585b6c87ddd59d65b3e1523b6b304376c7
iVGeek <48223278+iVGeek@users.noreply.github.com>
2023-03-19 12:42:42 +0300

    0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+python script that lists all states from the database hbtn_0e_0_usa
+"""
+
+import sys
+
+if __name__ == "__main__":
+    import MySQLdb
+    if (len(sys.argv) == 4):
+        user = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+        conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=user,
+            passwd=password,
+            db=database,
+            charset="utf8")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        conn.close()
