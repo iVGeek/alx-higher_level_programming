a52e5756d196d29860da4fce129e91004af07100
iVGeek <48223278+iVGeek@users.noreply.github.com>
2023-03-30 20:44:10 +0300

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""contains the function find_peak"""
+''' Technical interview preparation: find peak in lowest time complexity'''
 
 
 def find_peak(list_of_integers):
-    """finds a peak in a list of unsorted integers"""
-    li = list_of_integers
-    l = len(li)
-    if l == 0:
-        return
-    m = l // 2
-    if (m == l - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
-        return li[m]
-    if m != l - 1 and li[m + 1] > li[m]:
-        return find_peak(li[m + 1:])
-    return find_peak(li[:m])
+    ''' Finds a peak in a list of unsorted integers. '''
+    l = list_of_integers
+    return max(l) if l else None
+
+
+if __name__ == '__main__':
+    print(find_peak([1, 2, 4, 6, 3]))
+    print(find_peak([4, 2, 1, 2, 3, 1]))
+    print(find_peak([2, 2, 2]))
+    print(find_peak([]))
+    print(find_peak([-2, -4, 2, 1]))
+    print(find_peak([4, 2, 1, 2, 3, 1]))
