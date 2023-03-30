170a7f04afe3984ec61a2032ec5f1f7c899bfe6d
iVGeek <48223278+iVGeek@users.noreply.github.com>
2023-03-30 20:39:07 +0300

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-# finds peak
-
-
-def search(lo, h, ints):
-    mid = (lo + h) // 2
-    if lo == h:
-        return ints[h]
-    if ints[mid] < ints[mid + 1]:
-        return(search(mid + 1, h, ints))
-    return(search(lo, mid, ints))
+"""contains the function find_peak"""
 
 
 def find_peak(list_of_integers):
-    if not list_of_integers:
+    """finds a peak in a list of unsorted integers"""
+    li = list_of_integers
+    l = len(li)
+    if l == 0:
         return
-    return(search(0, len(list_of_integers) - 1, list_of_integers))
+    m = l // 2
+    if (m == l - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
+        return li[m]
+    if m != l - 1 and li[m + 1] > li[m]:
+        return find_peak(li[m + 1:])
+    return find_peak(li[:m])
